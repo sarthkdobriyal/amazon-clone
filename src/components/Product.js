50de7d7cb636e5a35a555a9166598fcfd713faa4
high-prog <dobriyalsarthaksa.8@gmail.com>
2023-01-26 01:18:08 +0530

    completed homepage

--- src/components/Product.js
+++ src/components/Product.js
@@ -18,42 +18,48 @@ function Product({id , title, price, description, category, image}) {
     //Randomly generated if prime or not
     const [prime, setPrime] = useState(
         //If less than 0.5 means true otherwise false
-        Math.floor(Math.random() < 0.5)
+        Math.random() < 0.5? true :false
     )
 
 
 
 
   return (
-    <div>
+    <div className='relative flex flex-col m-5 bg-white z-30 p-10 rounded-lg '>
         {/* category */}
-        <p>
+        <p className='absolute top-2 right-2 text-xs italic text-gray-400 tracking-widest '>
             {category}
         </p>
         
-        <Image width={200} height={200} style={{objectFit:"contain"}} src={image}  />
+        <Image width={200} height={200} style={{objectFit:"contain"}} src={image} className='mx-auto my-4'  />
         
-        <h4>{title}</h4>
+        <h4 className='my-3 font-bold tracking-wide'>{title}</h4>
 
         <div className='flex'>
             {
                 //Creating empty array of size rating to generate stars
                 Array(rating).fill().map((_, i) => (
-                    <StarIcon key={i} className="h-5 text-yellow-600" /> 
+                    <StarIcon key={i} className="h-6 text-yellow-600" /> 
                 ))
             }
         </div>
 
-        <p>{description}</p>
+        <p className='text-xs  my-2 line-clamp-2'>{description}</p>
 
-        <div>
+        <div className='mb-5'>
             <Currency quantity={price * 78} currency="INR" />
         </div>
 
         {
             prime &&
-            <img src="https://whitebox.com/wp-content/uploads/2020/05/Prime-tag-.png" alt="" />
+
+            (<div className='flex items-center space-x-2 -mt-5 '>
+            <img className='w-12' src="https://whitebox.com/wp-content/uploads/2020/05/Prime-tag-.png" alt="" />
+            <p className='text-xs text-gray-500'>FREE Next-Day Delivery</p>
+            </div>)
         }
+
+        <button className='mt-auto button'>Add to Basket</button>
 
     </div>
   )
--- src/components/Productfeed.js
+++ src/components/Productfeed.js
@@ -3,9 +3,12 @@ import Product from './Product'
 
 function Productfeed({ products }) {
   return (
-    <div className=''>
+    <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
+
+        {/* //sliing up the array to display the ad in the middle */}
+
         {
-            products.map(({id , title, price, description, category, image, rating}) => (
+            products.slice(0,4).map(({id , title, price, description, category, image, rating}) => (
                 <Product key={id} 
                     id={id}
                     title={title}
@@ -16,7 +19,43 @@ function Productfeed({ products }) {
                     rating={rating}
                 />
             ))
+
         }
+        <img className='md:col-span-full' src="https://images-eu.ssl-images-amazon.com/images/G/02/SBP/2018/gateway/1110572_smb_gw_desktop_1500x300_lavolio_1x_uk._CB484123630_.jpg" alt="" />
+
+        <div className='md:col-span-2'>
+        {
+            products.slice(4,5).map(({id , title, price, description, category, image, rating}) => (
+                <Product key={id} 
+                    id={id}
+                    title={title}
+                    price={price}
+                    description={description}
+                    category={category}
+                    image={image}
+                    rating={rating}
+                />
+            ))
+
+        }
+        </div>
+
+        {
+            products.slice(5,).map(({id , title, price, description, category, image, rating}) => (
+                <Product key={id} 
+                    id={id}
+                    title={title}
+                    price={price}
+                    description={description}
+                    category={category}
+                    image={image}
+                    rating={rating}
+                />
+            ))
+
+        }
+
+
     </div>
   )
 }
