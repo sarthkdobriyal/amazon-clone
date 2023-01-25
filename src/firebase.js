4c17cb79d579f7ba4e52161b96eee5ebfff32792
high-prog <dobriyalsarthaksa.8@gmail.com>
2023-01-26 02:01:39 +0530

    login functionality

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDYJlXRP-xnlR16D7nvLRcRNqTNgxZ4jBI",
+    authDomain: "amzn-clone-2efcb.firebaseapp.com",
+    projectId: "amzn-clone-2efcb",
+    storageBucket: "amzn-clone-2efcb.appspot.com",
+    messagingSenderId: "882680982467",
+    appId: "1:882680982467:web:62570d1a5915aac1f26b76"
+  };
