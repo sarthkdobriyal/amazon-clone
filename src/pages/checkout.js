6a4a8062c40960a415ac5951771316bb3eab7c9e
high-prog <dobriyalsarthaksa.8@gmail.com>
2023-01-31 07:14:25 +0530

    login?logout facility added

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,6 +1,6 @@
 import Header from '@/components/Header'
 import React from 'react'
-import Image from 'next/Image';
+import Image from 'next/image';
 
 function checkout() {
   return (
@@ -19,7 +19,7 @@ function checkout() {
 
                 <div className='flex flex-col p-5 space-y-10 bg-white'>
                     <h1 className='text-2xl border-b pb-4'>Your Shopping Basket</h1>
-                    
+
 
 
 
